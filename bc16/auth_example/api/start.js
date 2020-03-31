8988b632a924454f66383c8c30c27d579c557a93
Mykola Levkiv <reloaderlev@meta.ua>
2020-03-31 20:21:19 +0300

    deploy heroku auth example for bc16

--- bc16/auth_example/api/start.js
+++ bc16/auth_example/api/start.js
@@ -1,5 +1,13 @@
+// 1. heroku create
+// 2. npm start script
+// 3. get PORT from process.env.PORT
+// 4. deploy to heroku `git push heroku master`
+
 require = require("esm")(module);
 const { QuestionsServer } = require("./server");
 const { config } = require("./config");
+require("dotenv").config();
+
+console.log("process.env", process.env);
 
 new QuestionsServer(config).start();
