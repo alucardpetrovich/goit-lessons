9d4e1d6cd47911ca5d24394a0083f048e29b60f5
Mykola Levkiv <reloaderlev@gmail.com>
2021-01-23 15:11:14 +0200

    add 3-4 lessons for bc24

--- bc24/3_weather_server/server.js
+++ bc24/3_weather_server/server.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 import axios from "axios";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 
 const { pathname } = new URL(import.meta.url);
 const __dirname = path.dirname(pathname);
@@ -10,10 +11,43 @@ const __dirname = path.dirname(pathname);
 dotenv.config({ path: path.join(__dirname, ".env") });
 const server = express();
 
+// BETTER DO NOT USE THIS!!!!!!!
+// server.use(cors());
+
+server.use(
+  cors({
+    origin: function (origin, callback) {
+      const whitelist = process.env.ALLOWED_ORIGINS.split(",");
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+/*
 server.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 });
+
+server.options((req, res) => {
+  res.set(
+    "Access-Control-Allow-Methods",
+    req.get("Access-Control-Request-Method")
+  );
+  res.set(
+    "Access-Control-Allow-Headers",
+    req.get("Access-Control-Allow-Headers")
+  );
+  res.set("Access-Control-Allow-Credentials", "true");
+
+  res.send();
+});
+*/
+
+server.use(express.json());
 
 server.get("/weather", validateGetWeather, async (req, res) => {
   const { ln, lat } = req.query;
